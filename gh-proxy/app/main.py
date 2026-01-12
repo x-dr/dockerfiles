f38f16800de91d93d7defa368e0fb148bc5b4de8
x-dr <79109673+x-dr@users.noreply.github.com>
2026-01-13 00:05:53 +0800

    Update ASSET_URL to new GitHub Pages link

--- gh-proxy/app/main.py
+++ gh-proxy/app/main.py
@@ -33,7 +33,7 @@ pass_list = '''
 
 HOST = '127.0.0.1'  # 监听地址，建议监听本地然后由web服务器反代
 PORT = 80  # 监听端口
-ASSET_URL = 'https://hunshcn.github.io/gh-proxy'  # 主页
+ASSET_URL = 'https://x-dr.github.io/ghproxy'  # 主页
 
 white_list = [tuple([x.replace(' ', '') for x in i.split('/')]) for i in white_list.split('\n') if i]
 black_list = [tuple([x.replace(' ', '') for x in i.split('/')]) for i in black_list.split('\n') if i]
